feacdd02d02f90f377e94cf087c14890763f0774
Miguel Campos <camposmiguel@gmail.com>
2016-09-27 19:53:13 +0200

    LoginScreen

--- 02_Layouts/02_Calculator/app/src/main/java/com/miguelcr/linearlayout/MainActivity.java
+++ 02_Layouts/02_Calculator/app/src/main/java/com/miguelcr/linearlayout/MainActivity.java
@@ -3,6 +3,8 @@ package com.miguelcr.linearlayout;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,10 +17,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         screen = (TextView)findViewById(R.id.textViewScreen);
-        screen.setText("0.0");
+        screen.setText("0");
 
         Typeface type = Typeface.createFromAsset(getAssets(),"digital.ttf");
         screen.setTypeface(type);
 
+    }
+    public void numberButtonClicked(View view) {
+        int idButton = view.getId();
+        Button btnSelected = (Button)view;
+
+        writeNumber(btnSelected.getText().toString());
+    }
+    public void writeNumber(String number) {
+        if(screen.getText().toString().equals("0")){
+            screen.setText(number);
+        } else {
+            screen.setText(screen.getText() + number);
+        }
+    }
+
+    public void eraseClicked(View view) {
+        String screenText = screen.getText().toString();
+        String newScreenText = screenText.substring(0,screenText.length()-1);
+        screen.setText(newScreenText);
     }
 }
